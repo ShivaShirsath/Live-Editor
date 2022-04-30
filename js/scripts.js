02ec7cb1afafae782ac09a4067338e03428df0e8
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2022-04-30 17:50:36 +0530

    Functionality added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-function run() {
-    alert()
-}
+page = (document.getElementsByTagName('iframe')[0])
+page.contentWindow.document
+    .write(
+        '<style>' +
+        (
+            document.getElementsByTagName('textarea')[1]
+        ).value +
+        '</style>' +
+        (
+            document.getElementsByTagName('textarea')[0]
+        ).value
+    )
+    page.contentWindow.eval(
+        (
+            document.getElementsByTagName('textarea')[2]
+        ).value )
