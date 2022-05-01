8ad315d0b3dfd44eb7b249a52e6e5d9427d2ed98
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2022-05-01 17:19:24 +0530

    Codemirror added

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,60 @@
+view = document.querySelector("iframe")
+row = document.querySelector("row")
+column = document.querySelector("column")
+
 const html = CodeMirror(document.querySelector("html-code"), {
     lineNumbers: true,
+    theme: "dracula",
     tabSize: 2,
     mode: "xml",
+    autoCloseTags: true,
+    autoCloseBrackets: true
 });
 const css = CodeMirror(document.querySelector("css-code"), {
     lineNumbers: true,
+    theme: "dracula",
     tabSize: 2,
-    mode: "css"
+    mode: "css",
+    autoCloseTags: true,
+    autoCloseBrackets: true
 });
 const js = CodeMirror(document.querySelector("js-code"), {
     lineNumbers: true,
+    theme: "dracula",
     tabSize: 2,
-    mode: "javascript"
+    mode: "javascript",
+    autoCloseTags: true,
+    autoCloseBrackets: true
 });
 
+htmlElement = document.querySelector("html")
+
+width = htmlElement.offsetWidth
+height = htmlElement.offsetHeight
+h=1
+w=1
+if (height <width) {
+    h = .33
+    w=.5
+    row.style = "flex-direction: row"
+    column.style = "flex-direction:column"
+
+} 
+if(height>width) {
+    h = .5
+    w=.33
+    row.style = "flex-direction:column"
+
+        column.style = "flex-direction:row" 
+
+}
+
+    html.setSize(width *w, height*h)
+    css.setSize(width *w, height*h)
+    js.setSize(width *w, height*h)
 function view() {
-    let view = document.querySelector("iframe").contentWindow.document;
-    view.open();
-    view.write(
+    view.contentWindow.document.open();
+    view.contentWindow.document.write(
         "<style>" +
         css.getValue() +
         "</style>" +
@@ -26,5 +63,5 @@ function view() {
         js.getValue() +
         "</scr" + "ipt>"
     );
-    view.close();
+    view.contentWindow.document.close();
 }
