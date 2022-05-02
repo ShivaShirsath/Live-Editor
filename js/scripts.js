f63cb70d5039d8538d61777ae4776bd7048991f7
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2022-05-02 09:23:27 +0530

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,48 @@
-htmlElement = document.querySelector("html")
-view = document.querySelector("iframe")
-row = document.querySelector("row")
-column = document.querySelector("column")
-
-html = CodeMirror(document.querySelector("html-code"), {
-    lineNumbers: true,
-    theme: "dracula",
-    tabSize: 2,
-    mode: "xml",
-    autoCloseTags: true,
-    autoCloseBrackets: true
+const html = CodeMirror(document.querySelector("html-code"), {
+  lineNumbers: true,
+  theme: "dracula",
+  tabSize: 2,
+  mode: "xml",
+  autoCloseTags: true,
+  autoCloseBrackets: true,
 });
-css = CodeMirror(document.querySelector("css-code"), {
-    lineNumbers: true,
-    theme: "dracula",
-    tabSize: 2,
-    mode: "css",
-    autoCloseTags: true,
-    autoCloseBrackets: true
+const css = CodeMirror(document.querySelector("css-code"), {
+  lineNumbers: true,
+  theme: "dracula",
+  tabSize: 2,
+  mode: "css",
+  autoCloseTags: true,
+  autoCloseBrackets: true,
 });
-js = CodeMirror(document.querySelector("js-code"), {
-    lineNumbers: true,
-    theme: "dracula",
-    tabSize: 2,
-    mode: "javascript",
-    autoCloseTags: true,
-    autoCloseBrackets: true
+const js = CodeMirror(document.querySelector("js-code"), {
+  lineNumbers: true,
+  theme: "dracula",
+  tabSize: 2,
+  mode: "javascript",
+  autoCloseTags: true,
+  autoCloseBrackets: true,
 });
 
-width = htmlElement.offsetWidth
-height = htmlElement.offsetHeight
+width = document.offsetHeight;
+height = document.offsetHeight;
 
-h=1
-w=1
+html.setSize(width * 0.5, height * 0.33);
+css.setSize(width * 0.5, height * 0.33);
+js.setSize(width * 0.5, height * 0.33);
 
-if (height<width) {
-    h = .33
-    w=.5
-    row.style = "flex-direction: row"
-    column.style = "flex-direction:column"
-
-} 
-if(height>width) {
-    h = .5
-    w=.33
-    row.style = "flex-direction:column"
-    column.style = "flex-direction:row" 
-
-}
-
-    html.setSize(width *w, height*h)
-    css.setSize(width *w, height*h)
-    js.setSize(width *w, height*h)
-    
 function view() {
-    output = view.contentWindow.document;
-    output.open();
-    output.write(
-        "<style>" +
-        css.getValue() +
-        "</style>" +
-        html.getValue() +
-        "<scr" + "ipt>" +
-        js.getValue() +
-        "</scr" + "ipt>"
-    );
-    output.close();
+  let view = document.querySelector("iframe").contentWindow.document;
+  view.open();
+  view.write(
+    "<style>" +
+      css.getValue() +
+      "</style>" +
+      html.getValue() +
+      "<scr" +
+      "ipt>" +
+      js.getValue() +
+      "</scr" +
+      "ipt>"
+  );
+  view.close();
 }
