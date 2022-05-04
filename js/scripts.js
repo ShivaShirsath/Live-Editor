321f4e27c4fd021feb15c9befe24f30bafac4025
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-05-05 00:25:51 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -38,20 +38,53 @@ function view() {
       "<body"   + ">" + html.getValue() + "</body"   + ">" +
       "<script" + ">" +   js.getValue() + "</script" + ">"
   );
-  if (html.getValue().includes("<!-- save -->")) {
-      html.setValue(html.getValue().replace("<!-- save -->",''));
-      $.ajax({
-        method: "POST",
-        url: "php/code.php",
-        data: { 
-            html: html.getValue(),
-            css: css.getValue(),
-            js: js.getValue()
-        }
-      }).done(function( response ) {
-        alert(response);
-      });
+  if (html.getValue().includes("<!-- save -->")) {   
+    saveAction(html, "<!-- save -->", "html");
   }
-  
+  if (css.getValue().includes("/* save */")){
+    saveAction(css, "/* save */", "css");
+  }
+  if (js.getValue().includes("// save //")){
+    saveAction(js, "// save //", "js");
+  }
   view.close();
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+
+function saveAction(code, comment, ex){
+  code.setValue(code.getValue().replace(comment,''));
+  if(confirm("Do you want to upload files to server ?\n( 'Cancel' for download"))
+    $.ajax({
+      method: "POST",
+      url: "php/code.php",
+      data: { 
+        html: html.getValue(),
+        css: css.getValue(),
+        js: js.getValue()
+      }
+    }).done(function(response) {
+      alert(response);
+    });
+  else if (confirm("Save All code ?")) {
+      download(prompt("Download index file.", "index") + ".html" , 
+          "<html>\n\t<head>\n\t\t<title>Live Editor Presents</title>\n\t</head>\n\t<body>\n\t\t<style>" + 
+          css.getValue() + 
+          "\n\t\t</style>\n\t" +
+          html.getValue() + 
+          "\n\t\t<script>\n" +
+          js.getValue() +
+          "\n\t\t</script>\n\t</body>\n</html>"
+      );
+  } else  
+      download(prompt("Download " + ex + " file.", ex) + "." + ex, code.getValue());
 }
