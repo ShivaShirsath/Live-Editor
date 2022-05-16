99cc02ebfa4b249d339e21086fd42b289fa94ca0
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-05-16 08:29:26 +0000

    Formatted on github dev

--- js/scripts.js
+++ js/scripts.js
@@ -34,58 +34,78 @@ function view() {
   let view = document.querySelector("iframe").contentWindow.document;
   view.open();
   view.write(
-      "<style"  + ">" +  css.getValue() + "</style"  + ">" +
-      "<body"   + ">" + html.getValue() + "</body"   + ">" +
-      "<script" + ">" +   js.getValue() + "</script" + ">"
+    "<style" +
+      ">" +
+      css.getValue() +
+      "</style" +
+      ">" +
+      "<body" +
+      ">" +
+      html.getValue() +
+      "</body" +
+      ">" +
+      "<script" +
+      ">" +
+      js.getValue() +
+      "</script" +
+      ">"
   );
-  if (html.getValue().includes("<!-- save -->")) {   
+  if (html.getValue().includes("<!-- save -->")) {
     saveAction(html, "<!-- save -->", "html");
   }
-  if (css.getValue().includes("/* save */")){
+  if (css.getValue().includes("/* save */")) {
     saveAction(css, "/* save */", "css");
   }
-  if (js.getValue().includes("// save //")){
+  if (js.getValue().includes("// save //")) {
     saveAction(js, "// save //", "js");
   }
   view.close();
 }
 
 function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
 }
 
-
-function saveAction(code, comment, ex){
-  code.setValue(code.getValue().replace(comment,''));
-  if(confirm("Do you want to upload files to server ?\n( 'Cancel' for download"))
+function saveAction(code, comment, ex) {
+  code.setValue(code.getValue().replace(comment, ""));
+  if (
+    confirm("Do you want to upload files to server ?\n( 'Cancel' for download")
+  )
     $.ajax({
       method: "POST",
       url: "php/code.php",
-      data: { 
+      data: {
         html: html.getValue(),
         css: css.getValue(),
         js: js.getValue(),
-        command: 'insert'
-      }
-    }).done(function(response) {
+        command: "insert",
+      },
+    }).done(function (response) {
       alert(response);
     });
   else if (confirm("Save All code ?")) {
-      download(prompt("Download index file.", "index") + ".html" , 
-          "<html>\n\t<head>\n\t\t<title>Live Editor Presents</title>\n\t</head>\n\t<body>\n\t\t<style>" + 
-          css.getValue() + 
-          "\n\t\t</style>\n\t" +
-          html.getValue() + 
-          "\n\t\t<script>\n" +
-          js.getValue() +
-          "\n\t\t</script>\n\t</body>\n</html>"
-      );
-  } else  
-      download(prompt("Download " + ex + " file.", ex) + "." + ex, code.getValue());
+    download(
+      prompt("Download index file.", "index") + ".html",
+      "<html>\n\t<head>\n\t\t<title>Live Editor Presents</title>\n\t</head>\n\t<body>\n\t\t<style>" +
+        css.getValue() +
+        "\n\t\t</style>\n\t" +
+        html.getValue() +
+        "\n\t\t<script>\n" +
+        js.getValue() +
+        "\n\t\t</script>\n\t</body>\n</html>"
+    );
+  } else
+    download(
+      prompt("Download " + ex + " file.", ex) + "." + ex,
+      code.getValue()
+    );
 }
